4adb77c43634fa97d4b5c49a161f49baad547f81
Seth <Sethlee99@gmail.com>
2018-01-11 21:22:53 +0100

    even more bug fix

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,8 +1,8 @@
 # Be sure to restart your server when you modify this file.
-
+Rails.application.config.assets.precompile += %w( stylish_portfolio/manifest.css stylish_portfolio/manifest.js )
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
-Rails.application.config.assets.precompile += %w( stylish_portfolio/manifest.css stylish_portfolio/manifest.js )
+
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
 
